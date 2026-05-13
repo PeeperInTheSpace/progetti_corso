0f3470175d26e0e6098927a088230ab8b344393c
PeeperInTheSpace <107284763+PeeperInTheSpace@users.noreply.github.com>
2026-05-13 12:18:51 +0200

    Create main.py
    Aggiunto script iniziale

---
+++ main.py
@@ -0,0 +1,10 @@
+def saluta():
+    print("--- Benvenuti nel Calcolatore Collaborativo del Corso ---")
+
+def somma(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    saluta()
+    risultato = somma(5, 3)
+    print(f"Risultato della somma (5 + 3): {risultato}")
